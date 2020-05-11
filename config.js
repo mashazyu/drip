2c8c2b7db28644e535e5805f8d4b84ea58890699
mashazyu <mariya.z@gmail.com>
2020-05-11 16:39:31 +0200

    Introduces Chart main page redesign

--- config.js
+++ config.js
@@ -1,17 +1,11 @@
 const config = {
-  columnWidth: 25,
-  xAxisHeightPercentage: 0.08,
-  xAxisHeightPercentageLarge: 0.12,
-  symptomHeightPercentage: 0.05,
-  symptomHeightPercentageLarge: 0.1,
   temperatureScale: {
     defaultLow: 35,
     defaultHigh: 38,
     min: 34,
     max: 40,
     step: 0.5,
-    units: 0.1,
-    verticalPadding: 0.03
+    units: 0.1
   },
   symptoms: [
     'bleeding',
@@ -25,10 +19,30 @@ const config = {
   ],
 }
 
-config.columnMiddle = config.columnWidth / 2
-
 export const ACTION_DELETE = 'delete'
 export const ACTION_EXPORT = 'export'
 export const ACTION_IMPORT = 'import'
+
+export const SYMPTOMS = [
+  'bleeding',
+  'mucus',
+  'cervix',
+  'sex',
+  'desire',
+  'pain',
+  'mood',
+  'note'
+]
+
+// chart configuration
+export const COLUMN_WIDTH = 32
+export const COLUMN_MIDDLE = COLUMN_WIDTH / 2
+export const DOT_RADIUS = 6
+export const GRID_LINE_HORIZONTAL_WIDTH = 0.3
+export const ICON_SIZE = 20
+export const STROKE_WIDTH = 3
+export const XAXISHEIGHTRATIO = 0.14
+export const SYMPTOMHEIGHTRATIO = 0.1
+export const YAXIS_WIDTH = 32
 
 export default config
